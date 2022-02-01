56c5d4e148e0faaf1b5c19ac30d9149ee6f90800
Fatih Tatoğlu <fatih@tatoglu.net>
2022-02-01 21:48:13 +0300

    add local storage for saving settings

--- js/main.js
+++ js/main.js
@@ -16,37 +16,62 @@ window.onload = function () {
                 subMenu.style.display = "none";
             });
         }
+
     });
 
-    $html = document.getElementsByTagName("html")[0];
+    var $html = document.getElementsByTagName("html")[0];
 
-    $lnkSmallTheme = document.getElementById("lnkSmallTheme");
-    $lnkMediumTheme = document.getElementById("lnkMediumTheme");
-    $lnkLargeTheme = document.getElementById("lnkLargeTheme");
-    $lnkThemeLight = document.getElementById("lnkThemeLight");
-    $lnkThemeDark = document.getElementById("lnkThemeDark");
+    var $lnkSmallTheme = document.getElementById("lnkSmallTheme");
+    var $lnkMediumTheme = document.getElementById("lnkMediumTheme");
+    var $lnkLargeTheme = document.getElementById("lnkLargeTheme");
+    var $lnkThemeLight = document.getElementById("lnkThemeLight");
+    var $lnkThemeDark = document.getElementById("lnkThemeDark");
 
     // default
-    $html.setAttribute("data-theme", "light");
-    $html.setAttribute("data-width", "");
+    var themeWidth = window.localStorage.getItem("theme-width");
+    var themeColor = window.localStorage.getItem("theme-color");
+
+    if (themeWidth) {
+        $html.setAttribute("data-width", themeWidth);
+    }
+    else {
+        $html.setAttribute("data-width", "");
+    }
+
+    if (themeColor) {
+        $html.setAttribute("data-theme", themeColor);
+    }
+    else {
+        $html.setAttribute("data-theme", "light");
+    }
 
     $lnkSmallTheme.addEventListener("click", function () {
         $html.setAttribute("data-width", "small");
+
+        window.localStorage.setItem("theme-width", "small");
     });
 
     $lnkMediumTheme.addEventListener("click", function () {
         $html.setAttribute("data-width", "");
+
+        window.localStorage.setItem("theme-width", "");
     });
 
     $lnkLargeTheme.addEventListener("click", function () {
         $html.setAttribute("data-width", "large");
+
+        window.localStorage.setItem("theme-width", "large");
     });
 
     $lnkThemeLight.addEventListener("click", function () {
         $html.setAttribute("data-theme", "light");
+
+        window.localStorage.setItem("theme-color", "light");
     });
 
     $lnkThemeDark.addEventListener("click", function () {
         $html.setAttribute("data-theme", "dark");
+
+        window.localStorage.setItem("theme-color", "dark");
     });
 };
